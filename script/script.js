b04fdbda10d440e911b309c321a0036daecaa574
Stevelopes <Scubastevo8@hotmail.co.uk>
2021-04-06 21:56:47 +0100

    media queries

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', getLeagueData);
 
 async function getLeagueData() {
-    const response = await fetch('https://prod-relapi.ewp.gg/persisted/gw/getLeagues?h1=en-GB&leagueId=98767991302996019', {
+    const response = await fetch('https://prod-relapi.ewp.gg/persisted/gw/getSchedule?hl=en-GB&leagueId=98767991302996019', {
       headers: {
         'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'
       }
